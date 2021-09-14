3f36418f99e2cfe3e8f2395492b18dd5101129eb
zoro <zoro.dev@vmo.com>
2021-09-14 15:03:19 +0700

    update tailwind to work

--- src/page/ProgressPlanning.tsx
+++ src/page/ProgressPlanning.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { cleanup } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
 
 function ProgressPlanning() {
-    return (
-        <div>
-            Do planning here
-        </div>
-    )
+  // Minesweeper
+  //generate field
+  const [field, setField] = useState({
+    data: [12],
+  });
+  function addValue() {
+    setField({ ...field, data: [122, 12] });
+  }
+  useEffect(() => {
+    return () => {
+      cleanup();
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="p-4">Minefield have {field.data.length}</div>;
+      <button
+        onClick={() => {
+          addValue();
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
 }
 
-export default ProgressPlanning
+export default ProgressPlanning;
